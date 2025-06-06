b2e851b93057de8a7a98dd78e022fb50e6f18c09
Pruthviraj Solase <pruthvirajsolase37@gmail.com>
2025-06-06 11:44:56 +0530

    Comment for the saving data

---
+++ size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+     int a = 6; int b =0 ; int c=2147483648;
+    {
+        printf("Table of %d is\n", c);
+        
+    }
+   
+    for(int i=1 ; i<=10 ; i++)
+    {
+        printf("%d X %d = %d \n", a , b+=1 , a*i );
+     
+        
+    }
+
+return 0;
+}
