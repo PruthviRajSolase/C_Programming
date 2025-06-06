15b28159b45fda87f0abe30c55bc92073f6e3216
Pruthviraj Solase <pruthvirajsolase37@gmail.com>
2025-06-06 22:50:11 +0530

    Sum Calculator

--- Problem_1.c
+++ Problem_1.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-
 int main()
 {
+    int a , b ;
+       
+    printf("Sum Calculator, Enter the Numbers you want Sum of .....\n");
 
-    printf("Hello Pruthviraj");
+    printf("Enter Number a\n");
+    scanf("%d", &a);
 
+    printf("Enter Number b\n");
+    scanf("%d", &b);
+
+    printf("The Sum is Equal to %d", a+b);
+    
+
+
+    
     return 0;
 }
