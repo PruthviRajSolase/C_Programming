0e33f5d2f9c9b3fb0684f50e8cd2578d9906724b
Pruthviraj Solase <pruthvirajsolase37@gmail.com>
2025-06-25 23:10:09 +0530

    Array

--- Problem_1.c
+++ Problem_1.c
@@ -3,12 +3,11 @@
 
 int main()
 {
-    int a = 5 ;
-    int b = 98 ;
+  int num[] = {65,90,43,54,76,88};
+  printf("The Number is %d\n", num[2]);
+  printf("The Number is %d\n", num[3]);
+  printf("The Number is %d\n", num[4]);
+  printf("The Number is %d\n", num[0]);
 
-    printf("%d\n", a);
-    printf("%d\n", b);
-
-    
     return 0;
 }
