af38f8674e4f0b997ec2203bc2ef154507c90a6f
Pruthviraj Solase <pruthvirajsolase37@gmail.com>
2025-06-29 12:49:40 +0530

    Use of Functions

--- Problem_1.c
+++ Problem_1.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
+void function1();
+void function2();
+
 
 int main()
 {
-  char name[] = {'P','r','u','t','h','v','i','r','a','j','\0'};
-  char SurName[] = "Solase";
-
-  printf("%s ",name);
-  printf("%s", SurName);
+  printf("In main function\n");
+  printf("Calling Function 1\n");
+  function1();
+  printf("In Main function after returning from function 1\n");
 
   return 0;
+}
+
+void function1()
+{
+  printf("You have Entered Function 1\n");
+  printf("Calling Function 2\n");
+  function2();
+
+}
+
+void function2()
+{
+  printf("You have Entered Function 2\n");
+  printf("Get back to Main Function through function 1\n");
 }
